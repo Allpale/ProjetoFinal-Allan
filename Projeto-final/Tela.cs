b3134b6df0a8ce17fe8ffd997195f87939565105
Allan P. Leme <allpale16@gmail.com>
2017-11-29 15:33:01 -0200

    Melhorando a implementacao do metodo imprimir Tabuleiro

--- Projeto-final/Tela.cs
+++ Projeto-final/Tela.cs
@@ -9,6 +9,7 @@ namespace Projeto_final
         {
             for (int i=0; i<tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j=0; j<tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,10 +18,30 @@ namespace Projeto_final
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
             }
         }
     }
